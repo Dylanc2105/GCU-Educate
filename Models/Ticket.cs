f000a3929f22d1dd44511a82047a1dbd5ef81e69
WilliamMMclean <williammmclean93@gmail.com>
2025-03-17 03:51:11 +0000

    Update Ticket.cs
    removal of FK's and added others to fit database schema.

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -12,23 +12,34 @@ namespace GuidanceTracker.Models
 
         public string TicketTitle { get; set; }
 
-        [Required]
         public string TicketDescription { get; set; }
-
         public string TicketStatus { get; set; } = "Open";
 
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; }
 
         // Foreign keys
+
+        [Required]
         public string LecturerId { get; set; }
-        public string StudentId { get; set; }
+
+        [ForeignKey("LecturerId")]
+        public virtual Lecturer Lecturer { get; set; }
+
+        [Required]
         public string GuidanceTeacherId { get; set; }
 
-        // Navigation properties
-        public virtual Lecturer Lecturer { get; set; }
-        public virtual Student Student { get; set; }
+        [ForeignKey("GuidanceTeacherId")]
         public virtual GuidanceTeacher GuidanceTeacher { get; set; }
+
+        [Required]
+        public string StudentId { get; set; }
+
+        [ForeignKey("StudentId")]
+        public virtual Student Student { get; set; }
+
+        // Optional reference to an archived ticket if this was restored
+        public int? ArchivedTicketId { get; set; }
 
         // 🔹 Adding a list of comments
         public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
