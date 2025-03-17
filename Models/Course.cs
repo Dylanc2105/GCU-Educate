eccb9ff64fb824c4d03308aa8c02578545cf726b
WilliamMMclean <williammmclean93@gmail.com>
2025-03-17 03:47:08 +0000

    Update Course.cs
    Updated course class to include data given to us. Also changed the collections and foreign keys.

--- Models/Course.cs
+++ Models/Course.cs
@@ -1,18 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 
 namespace GuidanceTracker.Models
 {
     public class Course
     {
+        [Key]
         public int CourseId { get; set; }
+
+        [Required]
+        [Display(Name = "Course Name")]
         public string CourseName { get; set; }
-        public string CourseDescription { get; set; }
+
+        [Required]
+        [Display(Name = "Course Reference")]
+        public string CourseReference { get; set; }
+
+        [Required]
+        [Display(Name = "Mode of Study")]
+        public string ModeOfStudy { get; set; }
+
+        [Display(Name = "Duration in Weeks")]
+        public int DurationInWeeks { get; set; }
+
+        [Display(Name = "SCQF Level")]
+        public int SCQFLevel { get; set; }
+        public string Site { get; set; }
+
+        [Required]
+        [Display(Name = "Start Date")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "End Date")]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
 
         // Navigation properties
-        public virtual ICollection<Module> Modules { get; set; } 
-        public virtual ICollection<Student> Students { get; set; } 
+
+        [Required]
+        public string DepartmentId { get; set; }
+
+        [ForeignKey("DepartmentId")]
+        public virtual Department Department { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
 }
