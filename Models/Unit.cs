986c2d99db81c3b5607b74e1c6bcb1d95b768700
WilliamMMclean <williammmclean93@gmail.com>
2025-03-17 03:51:25 +0000

    Create Unit.cs
    Created Unit in place of module class.

---
+++ Models/Unit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GuidanceTracker.Models
+{
+    public class Unit
+    {
+        public int UnitId { get; set; }
+        public string UnitName { get; set; }
+        public string UnitDescription { get; set; }
+
+        // Foreign keys
+        public ICollection<Class> Classes { get; set; }
+
+        [Required]
+
+        public string LecturerId { get; set; }
+
+        [ForeignKey("LecturerId")]
+
+        public virtual Lecturer Lecturer { get; set; }
+
+        public Unit()
+        {
+            Classes = new HashSet<Class>();
+        }
+
+
+
+    }
+}
