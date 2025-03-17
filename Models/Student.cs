b358f710ce2a260b9c29406d02c5b62c241b1a5f
WilliamMMclean <williammmclean93@gmail.com>
2025-03-17 03:50:44 +0000

    Update Student.cs
    added foreignkeys and collections

--- Models/Student.cs
+++ Models/Student.cs
@@ -8,18 +8,26 @@ using System.Net.Sockets;
 
 namespace GuidanceTracker.Models
 {
-    public class Student:User
+    public class Student : User
     {
         // Foreign key to GuidanceTeacher
         public string GuidanceTeacherId { get; set; }
         public virtual GuidanceTeacher GuidanceTeacher { get; set; }
 
-        // Foreign key to Course
-        public int CourseId { get; set; }
-        public virtual Course Course { get; set; }
+        [Required]
+        public int ClassId { get; set; }
 
-        // Navigation properties
-        public virtual ICollection<Session> Sessions { get; set; } 
-        public virtual ICollection<Ticket> Tickets { get; set; } 
+        [ForeignKey("ClassId")]
+        public virtual Class Class { get; set; }
+
+        public string FeedbackId { get; set; }
+
+        [ForeignKey("FeedbackId")]
+
+        public virtual Feedback Feedback { get; set; }
+
+        public ICollection<MessageBoard> MessageBoards { get; set; }
+        public ICollection<Appointment> Appointments { get; set; }
+
     }
 }
