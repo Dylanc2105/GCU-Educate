7d9ca58272a641323dbc161fd4b8d9ce7e397fac
WilliamMMclean <williammmclean93@gmail.com>
2025-03-17 03:43:08 +0000

    Appointment class
    added an appointment class

---
+++ Models/Appointment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GuidanceTracker.Models
+{
+	public class Appointment
+	{
+        public int AppointmentId { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public string AppointmentStatus { get; set; }/* = Requested;*/
+        public string AppointmentNotes { get; set; }
+
+        [Required]
+        public string GuidanceTeacherId { get; set; }
+
+        [ForeignKey("GuidanceTeacherId")]
+        public virtual GuidanceTeacher GuidanceTeacher { get; set; }
+
+        [Required]
+        public string StudentId { get; set; }
+
+        [ForeignKey("StudentId")]
+        public virtual Student Student { get; set; }
+    }
+
+    public enum AppointmentStatus
+    {
+        Requested,
+        Scheduled,
+        Completed,
+        Cancelled,
+        Rescheduled
+    }
+}
