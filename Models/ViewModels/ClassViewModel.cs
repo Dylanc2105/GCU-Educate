3772876bd580c5615480dea4479df80a2e8efab7
WilliamMMclean <williammmclean93@gmail.com>
2025-03-17 03:51:39 +0000

    Create ClassViewModel.cs

---
+++ Models/ViewModels/ClassViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GuidanceTracker.Models.ViewModels
+{
+    public class ClassViewModel
+    {
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+    }
+}
