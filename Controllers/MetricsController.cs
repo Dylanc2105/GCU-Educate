5be8404f5bca461ff3c8fd8e0c87ca92fc8ba3da
xKarin <karina.fatkullina@gmail.com>
2025-03-26 00:37:37 +0000

    initial metics are done, playing about with pie charts

--- Controllers/MetricsController.cs
+++ Controllers/MetricsController.cs
@@ -1,17 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using GuidanceTracker.Models;
 using GuidanceTracker.Models.ViewModels;
-using static GuidanceTracker.Models.ViewModels.MetricsViewModel;
 using System.Data.Entity;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-
 
 public class MetricsController : Controller
 {
-   
+    private readonly GuidanceTrackerDbContext db = new GuidanceTrackerDbContext();
+
+    public ActionResult Index(int? classId)
+    {
+        // variable to hold issues
+        var issuesQuery = db.Issues.AsQueryable();
+
+        // if class is selected, filter issues for students in that class
+        if (classId.HasValue)
+        {
+            issuesQuery = issuesQuery.Where(i => i.Student.ClassId == classId.Value);
+        }
+
+        
+
+        var model = new MetricsViewModel
+        {
+            // total number of issues
+            TotalIssues = issuesQuery.Count(),
+
+            // issues by type (filtered)
+            IssuesByType = issuesQuery
+                .GroupBy(i => i.IssueTitle.ToString())
+                .Select(g => new IssueByType
+                {
+                    IssueType = g.Key,
+                    Count = g.Count()
+                }).ToList(),
+
+            // issues by class (filtered)
+            IssuesByClass = db.Classes
+                .Select(c => new IssueByClass
+                {
+                    ClassName = c.ClassName,
+                    IssueCount = issuesQuery
+                        .Count(i => i.Student.ClassId == c.ClassId)
+                }).ToList(),
+
+            // populated the class dropdown menu
+            Classes = db.Classes.ToList(),
+            // if provided set the selected class id
+            SelectedClassId = classId
+        };
+
+        return View(model);
+    }
 }
