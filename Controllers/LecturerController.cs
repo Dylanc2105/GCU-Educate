73883526f5addb04247d3778c8b7657402084ac8
xKarin <karina.fatkullina@gmail.com>
2025-04-15 10:23:08 +0100

    lecturer dash added

--- Controllers/LecturerController.cs
+++ Controllers/LecturerController.cs
@@ -1,6 +1,9 @@
 ﻿using GuidanceTracker.Models;
+using GuidanceTracker.Models.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,7 +19,21 @@ namespace GuidanceTracker.Controllers
         // GET: Lecturer
         public ActionResult LecturerDash()
         {
-            return View("LecturerDash");
+            var userId = User.Identity.GetUserId();
+            var user = db.Lecturers.Find(userId);
+            var today = DateTime.Today;
+
+            var model = new LecturerDashViewModel
+            {
+                FirstName = user.FirstName,
+                //CurrentDateTime = DateTime.Now,
+                NewProgressIssuesCount = db.Issues.Where(i => i.IssueStatus == IssueStatus.New).Count(),
+                //NewNotificationsCount = db.Notifications.Where(n => n.IsRead == false).Count(),
+                //NewAnnouncementsCount = db.Announcements.Where(a => a.IsRead == false).Count()
+
+
+            };
+            return View(model);
         }
 
         public ActionResult ViewAllStudents()
---
+++ Models/ViewModels/LecturerDashViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GuidanceTracker.Models.ViewModels
+{
+	public class LecturerDashViewModel
+	{
+        public string FirstName { get; set; }
+        public int NewProgressIssuesCount { get; set; }
+        public int NewNotificationsCount { get; set; }
+        public int NewAnnouncementsCount { get; set; }
+    }
+}
